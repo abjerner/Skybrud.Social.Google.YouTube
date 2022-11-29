ed910e4617c2476b34d25595e444f6f45bee6382
Anders Bjerner <abjerner@skybrud.dk>
2022-11-29 15:24:56 +0100

    #H5IS

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Skybrud.Social.Google.YouTube --configuration Release /t:rebuild /t:pack p:PackageOutputPath=../../releases/nuget
+dotnet build src/Skybrud.Social.Google.YouTube --configuration Release /t:rebuild /t:pack -p:PackageOutputPath=../../releases/nuget
