3db5cd77cf2ad1251839a71ec74c09b771dcd330
Anders Bjerner <abjerner@skybrud.dk>
2022-11-29 09:16:50 +0100

    Added static methods for creating YouTubeHttpService instances directly

--- src/Skybrud.Social.Google.YouTube/YouTubeHttpService.cs
+++ src/Skybrud.Social.Google.YouTube/YouTubeHttpService.cs
@@ -48,6 +48,44 @@ namespace Skybrud.Social.Google.YouTube {
 
         #endregion
 
+        #region Static methods
+
+        /// <summary>
+        /// Initializes a new instance based on the specified <paramref name="apiKey"/>. The API key is tied to
+        /// your app, whereas an access token or refresh token is tied the a specific user of your app. Calling this
+        /// method will not make any calls to the Google Accounts API.
+        /// </summary>
+        /// <param name="apiKey">The API key of your app.</param>
+        /// <returns>An instance of <see cref="YouTubeHttpService"/>.</returns>
+        public static YouTubeHttpService CreateFromApiKey(string apiKey) {
+            return GoogleHttpService.CreateFromApiKey(apiKey).YouTube();
+        }
+
+        /// <summary>
+        /// Initializes a new instance based on an access token. Calling this method will not make any calls to the
+        /// Google Accounts API.
+        /// </summary>
+        /// <param name="accessToken">The access token.</param>
+        /// <returns>An instance of <see cref="YouTubeHttpService"/>.</returns>
+        public static YouTubeHttpService CreateFromAccessToken(string accessToken) {
+            return GoogleHttpService.CreateFromAccessToken(accessToken).YouTube();
+        }
+
+        /// <summary>
+        /// Initializes a new instance based on the specified refresh token. The refresh token is used for making a
+        /// call to the Google Accounts API to get a new access token. Access tokens typically expire after an hour
+        /// (3600 seconds).
+        /// </summary>
+        /// <param name="clientId">The client ID.</param>
+        /// <param name="clientSecret">The client secret.</param>
+        /// <param name="refreshToken">The refresh token of the user.</param>
+        /// <returns>An instance of <see cref="YouTubeHttpService"/>.</returns>
+        public static YouTubeHttpService CreateFromRefreshToken(string clientId, string clientSecret, string refreshToken) {
+            return GoogleHttpService.CreateFromRefreshToken(clientId, clientSecret, refreshToken).YouTube();
+        }
+
+        #endregion
+
     }
 
 }
